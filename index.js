b338f121829b2434135354f5ba88acdff1f34441
Pedro Henrique <pedrohenriqueteodorodacosta@gmail.com>
2017-11-08 03:33:56 -0200

    Added argument for "Start in"

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ function getName(path) {
 
 // For now it works only in a synchronous way
 // Asynchronous version of the function needs further development
-exports.makeSync = function (file, name, desc, icon) {
+exports.makeSync = function (file, name, desc, scwd, icon) {
     if (typeof file !== 'string' || fs.existsSync(file) === false) throw new Error('Invalid base file');
     if (typeof name !== 'string') name = getName(file) + ' - Shortcut';
     if (typeof desc !== 'string') desc = getName(file) + ' - Shortcut';
+    if (typeof scwd !== 'string') scwd = '';
     if (typeof icon !== 'string') icon = file;
-    child_process.execFileSync(__dirname + '\\exe.win-amd64-2.7\\run.exe', [file, name, desc, icon]);
+    child_process.execFileSync(__dirname + '\\exe.win-amd64-2.7\\run.exe', [file, name, desc, scwd, icon]);
 }
